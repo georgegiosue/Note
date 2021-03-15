cc5553544492999277d86353cc60a9365cc7ac59
george <peraldonamoc@gmail.com>
2021-03-15 12:14:04 -0500

    update-add start activity

--- app/src/main/java/com/nmrc/note/MainActivity.kt
+++ app/src/main/java/com/nmrc/note/MainActivity.kt
@@ -32,16 +32,17 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
+
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
---
+++ app/src/main/java/com/nmrc/note/StartApp.kt
@@ -0,0 +1,11 @@
+package com.nmrc.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_app)
+    }
+}
