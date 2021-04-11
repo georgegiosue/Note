b0a63f169db2fe3091cb9428dc543cf9247f51ea
george <peraldonamoc@gmail.com>
2021-04-10 20:18:58 -0500

    Main UI partially finished

--- app/src/main/java/com/nmrc/note/ui/fragments/TaskFragment.kt
+++ app/src/main/java/com/nmrc/note/ui/fragments/TaskFragment.kt
@@ -1,10 +1,10 @@
 package com.nmrc.note.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.nmrc.note.R
 
 
@@ -14,6 +14,8 @@ class TaskFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.fragment_task, container, false)
+
+
     }
 
 
