7e70426c76a93ba86d32fd23b72dbc8d12314ce7
george <peraldonamoc@gmail.com>
2021-04-12 19:40:36 -0500

    add Dialogs Fragments: Note and Task

---
+++ app/src/main/java/com/nmrc/note/ui/fragments/dialogs/NewTaskDialogFragment.kt
@@ -0,0 +1,18 @@
+package com.nmrc.note.ui.fragments.dialogs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nmrc.note.R
+
+class NewTaskDialogFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_new_task_dialog, container, false)
+    }
+
+
+}
