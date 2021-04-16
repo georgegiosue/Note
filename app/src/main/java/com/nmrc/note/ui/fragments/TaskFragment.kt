a082bc9b4d1d61d5cbceb83f8bad496d6da2dbc9
george <peraldonamoc@gmail.com>
2021-04-16 00:06:38 -0500

    toNewTask action completed

--- app/src/main/java/com/nmrc/note/ui/fragments/TaskFragment.kt
+++ app/src/main/java/com/nmrc/note/ui/fragments/TaskFragment.kt
@@ -5,18 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.nmrc.note.R
+import com.nmrc.note.databinding.FragmentTaskBinding
 
 
 class TaskFragment : Fragment() {
 
+    private var _binding: FragmentTaskBinding? = null
+    private val binding get() = _binding!!
+    private var navController: NavController? = null
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_task, container, false)
+        _binding = FragmentTaskBinding.inflate(inflater,container,false)
 
-
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        newTask()
+    }
+
+    private fun newTask() {
+
+        binding.fabNewTask.setOnClickListener {
+            navController!!.navigate(R.id.action_toNewTask)
+        }
+    }
 
 }
