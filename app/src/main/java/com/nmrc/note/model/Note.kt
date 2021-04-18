07269f4e7932c4567666efcdec0c5c9c15387108
george <peraldonamoc@gmail.com>
2021-04-18 12:22:15 -0500

    Note model created

---
+++ app/src/main/java/com/nmrc/note/model/Note.kt
@@ -0,0 +1,10 @@
+package com.nmrc.note.model
+
+data class Note (
+
+        var title: String,
+        var date: String,
+        var description: String,
+        var favorite: Boolean,
+        var image: Int
+        )
